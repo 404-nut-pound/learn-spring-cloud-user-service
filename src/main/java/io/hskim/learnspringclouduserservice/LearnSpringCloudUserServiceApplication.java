65d356615caad0a07b5467fbcd3d84ff68579811
404-nut-pound <souls4523@gmail.com>
2023-07-21 23:32:30 +0900

    initial commit

---
+++ src/main/java/io/hskim/learnspringclouduserservice/LearnSpringCloudUserServiceApplication.java
@@ -0,0 +1,12 @@
+package io.hskim.learnspringclouduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudUserServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSpringCloudUserServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/io/hskim/learnspringclouduserservice/LearnSpringCloudUserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package io.hskim.learnspringclouduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudUserServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
