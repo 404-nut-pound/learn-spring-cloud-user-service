3d51159732eabe545ea9ce30ec0a772bbff43f65
404-nut-pound <souls4523@gmail.com>
2023-07-22 23:34:19 +0900

    [feat] 다중 인스턴스 확인을 위한 endpoint 추가

--- src/main/java/io/hskim/learnspringclouduserservice/UserController.java
+++ src/main/java/io/hskim/learnspringclouduserservice/UserController.java
@@ -1,15 +1,20 @@
 package io.hskim.learnspringclouduserservice;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
+  private final Environment env;
+
   @GetMapping(value = "/{serviceType}")
-  public String getFirstService(@PathVariable String serviceType) {
+  public String getServiceType(@PathVariable String serviceType) {
     return "Welcome to %s!".formatted(serviceType.replace("-", " "));
   }
 
@@ -23,5 +28,13 @@ public class UserController {
     ) String requestHeader
   ) {
     return requestHeader;
+  }
+
+  @GetMapping(value = "/{serviceType}/check")
+  public String getCheck(@PathVariable String serviceType) {
+    return "Check Application %s:%s".formatted(
+        env.getProperty("spring.application.name"),
+        env.getProperty("local.server.port")
+      );
   }
 }
