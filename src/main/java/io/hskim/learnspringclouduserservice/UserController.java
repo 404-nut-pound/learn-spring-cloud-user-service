906360f630c69af9bfee7c5ef8ed2995e56b8f23
404-nut-pound <souls4523@gmail.com>
2023-07-22 22:19:02 +0900

    [feat] Gateway에서 전달하는 커스텀 헤더를 출력하는 endpoint 추가

--- src/main/java/io/hskim/learnspringclouduserservice/UserController.java
+++ src/main/java/io/hskim/learnspringclouduserservice/UserController.java
@@ -1,6 +1,8 @@
 package io.hskim.learnspringclouduserservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,17 @@ public class UserController {
   @GetMapping(value = "/second-service")
   public String getSecondService() {
     return "Welcome to second service!";
+  }
+
+  @GetMapping(value = "/{serviceType}/message")
+  public String getMessage(
+    @PathVariable String serviceType,
+    @RequestHeader(
+      value = "gateway-request",
+      required = false,
+      defaultValue = "'gateway-request' header not-provided"
+    ) String requestHeader
+  ) {
+    return requestHeader;
   }
 }
