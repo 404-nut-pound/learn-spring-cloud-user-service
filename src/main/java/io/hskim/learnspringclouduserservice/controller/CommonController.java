af56c518f65d678edfb1e1f006cb458036f504c7
404-nut-pound <souls4523@gmail.com>
2023-07-24 02:12:00 +0900

    [test] Endpoint 중복 현상에 따라 임시 주석 처리

--- src/main/java/io/hskim/learnspringclouduserservice/controller/CommonController.java
+++ src/main/java/io/hskim/learnspringclouduserservice/controller/CommonController.java
@@ -1,42 +1,42 @@
-package io.hskim.learnspringclouduserservice.controller;
+// package io.hskim.learnspringclouduserservice.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.core.env.Environment;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestHeader;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/{serviceType}")
-@RequiredArgsConstructor
-public class CommonController {
+// @RestController
+// @RequestMapping(value = "/{serviceType}")
+// @RequiredArgsConstructor
+// public class CommonController {
 
-  private final Environment env;
+//   private final Environment env;
 
-  @GetMapping
-  public String getServiceType(@PathVariable String serviceType) {
-    return "Welcome to %s!".formatted(serviceType.replace("-", " "));
-  }
+//   @GetMapping
+//   public String getServiceType(@PathVariable String serviceType) {
+//     return "Welcome to %s!".formatted(serviceType.replace("-", " "));
+//   }
 
-  @GetMapping(value = "/message")
-  public String getMessage(
-    @PathVariable String serviceType,
-    @RequestHeader(
-      value = "gateway-request",
-      required = false,
-      defaultValue = "'gateway-request' header not-provided"
-    ) String requestHeader
-  ) {
-    return requestHeader;
-  }
+//   @GetMapping(value = "/message")
+//   public String getMessage(
+//     @PathVariable String serviceType,
+//     @RequestHeader(
+//       value = "gateway-request",
+//       required = false,
+//       defaultValue = "'gateway-request' header not-provided"
+//     ) String requestHeader
+//   ) {
+//     return requestHeader;
+//   }
 
-  @GetMapping(value = "/check")
-  public String getCheck(@PathVariable String serviceType) {
-    return "Check Application %s:%s".formatted(
-        env.getProperty("spring.application.name"),
-        env.getProperty("local.server.port")
-      );
-  }
-}
+//   @GetMapping(value = "/check")
+//   public String getCheck(@PathVariable String serviceType) {
+//     return "Check Application %s:%s".formatted(
+//         env.getProperty("spring.application.name"),
+//         env.getProperty("local.server.port")
+//       );
+//   }
+// }
