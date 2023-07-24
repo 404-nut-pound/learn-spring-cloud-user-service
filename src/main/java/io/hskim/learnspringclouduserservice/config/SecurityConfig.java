ced39987be0fa1c5585bde5ae5070e4e90468a5e
404-nut-pound <souls4523@gmail.com>
2023-07-24 19:05:56 +0900

    [feat] Spring Boot Actuator 추가

--- src/main/java/io/hskim/learnspringclouduserservice/config/SecurityConfig.java
+++ src/main/java/io/hskim/learnspringclouduserservice/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableMethodSecurity
@@ -47,6 +48,8 @@ public class SecurityConfig {
       .authorizeHttpRequests(request ->
         request
           .requestMatchers(PathRequest.toH2Console())
+          .permitAll()
+          .requestMatchers(AntPathRequestMatcher.antMatcher("/actuator/**"))
           .permitAll()
           .anyRequest()
           .permitAll()
